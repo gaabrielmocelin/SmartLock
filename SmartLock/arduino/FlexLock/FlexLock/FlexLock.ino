13d25f92236ddc5a15b287f87f992192a6dd356f
Rafael Grohmann Cadaval <rafaelcadaval@gmail.com>
2018-02-22 17:09:56 -0300

    Adds buzzer-command sending on Arduino

--- SmartLock/arduino/FlexLock/FlexLock/FlexLock.ino
+++ SmartLock/arduino/FlexLock/FlexLock/FlexLock.ino
@@ -19,10 +19,14 @@ int button2State = 0;
 int button3State = 0;
 
 unsigned long int lockUpdateTimestamp;
-boolean updatingLock = false;
+boolean isUpdatingLock = false;
 
 enum LockState { LOCKED, UNLOCKED };
 LockState state;
+
+const char LOCK_COMMAND = 'L';
+const char UNLOCK_COMMAND = 'U';
+const char BUZZER_SEND_COMMAND = 'B';
 
 void setup() {
   tranca.attach(portaTranca);
@@ -45,81 +49,88 @@ uint8_t byteRead = 0;
 int once = 0;
 
 void loop() {
-  if (updatingLock) {
-      if (millis() - lockUpdateTimestamp >= 600) {
-          updatingLock = false;
-          updateLockStatusColor();
-      }
+  if (isUpdatingLock) {
+    if (millis() - lockUpdateTimestamp >= 600) {
+      isUpdatingLock = false;
+      updateLockStatusColor();
+    }
   } else {
-      checkLockButton();
+    checkLockButton();
   }
-
-  checkBuzzerButton();
   
+  checkBuzzerButton();
+
   if (Serial.available() > 0) {
     byteRead = Serial.read();
     Serial.write(byteRead);
 
-    if (byteRead == 'U') {
+    if (byteRead == UNLOCK_COMMAND) {
       unlock();
-    } else if (byteRead == 'L') {
+    } else if (byteRead == LOCK_COMMAND) {
       lock();
     }
   }
 }
 
-void checkLockButton() {  
+void checkLockButton() {
   button1State = digitalRead(button1Pin);
   button2State = digitalRead(button2Pin);
-  
+
   if (button1State == LOW || button2State == LOW) {
-    switch(state) {
+    switch (state) {
       case LOCKED:
         unlock();
         break;
       case UNLOCKED:
         lock();
     }
-  }
+  } 
+
 }
 
 void checkBuzzerButton() {
   button3State = digitalRead(button3Pin);
-  
+
   if (button3State == LOW) {
     tone(piezoPin, 2000);
+    sendBuzzNotification();
   } else {
     noTone(piezoPin);
   }
 }
 
+void sendBuzzNotification() {
+    Serial.write(BUZZER_SEND_COMMAND);
+    delay(50);
+}
+
 void unlock() {
-    lockUpdateTimestamp = millis();
-    updatingLock = true;
-    tranca.write(0);
-    state = UNLOCKED;
+  lockUpdateTimestamp = millis();
+  isUpdatingLock = true;
+  tranca.write(0);
+  state = UNLOCKED;
 }
 
 void lock() {
-    lockUpdateTimestamp = millis();
-    updatingLock = true;
-    tranca.write(512);
-    state = LOCKED;
+  lockUpdateTimestamp = millis();
+  isUpdatingLock = true;
+  tranca.write(150);
+  state = LOCKED;
 }
 
 void updateLockStatusColor() {
-    int r, g, b = LOW;
-    switch(state) {
-      case UNLOCKED:
-        r = LOW;
-        g = HIGH;
-        break;
-      case LOCKED:
-        r = HIGH;
-        g = LOW;
-    }
-    digitalWrite(redPin, r);
-    digitalWrite(greenPin, g);
-    digitalWrite(bluePin, b);
+  int r, g, b = LOW;
+  switch (state) {
+    case UNLOCKED:
+      r = LOW;
+      g = HIGH;
+      break;
+    case LOCKED:
+      r = HIGH;
+      g = LOW;
+  }
+  digitalWrite(redPin, r);
+  digitalWrite(greenPin, g);
+  digitalWrite(bluePin, b);
 }
 
