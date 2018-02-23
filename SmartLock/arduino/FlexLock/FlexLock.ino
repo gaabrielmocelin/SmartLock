5bcba16ef388cac2f3ab909f3bd865b9aa2dd565
Max Zorzetti <maxzorzetti@live.com>
2018-02-23 17:42:12 -0300

    Adds unlock/lock timeout functions to Arduino
    Might be useful later on.
    Also added some empty functions related to the door opening/closing.

--- SmartLock/arduino/FlexLock/FlexLock.ino
+++ SmartLock/arduino/FlexLock/FlexLock.ino
@@ -18,11 +18,23 @@ int button1State = 0;
 int button2State = 0;
 int button3State = 0;
 
+// Lock stuff
 enum LockState { LOCKED, UNLOCKED };
-const unsigned long int LOCK_DELAY = 600;
 LockState state;
+
+const unsigned long int LOCK_UPDATE_DELAY = 600;
 unsigned long int lockUpdateTimestamp;
-boolean isUpdatingLock = false;
+boolean isLockUpdating = false;
+
+const unsigned long int LOCK_ACTION_TIMEOUT_DELAY = 5000;
+unsigned long int lockActionTimeoutTimestamp;
+boolean isLockActionOnTimeout = false;
+
+const unsigned long int UNLOCK_ACTION_TIMEOUT_DELAY = 5000;
+unsigned long int unlockActionTimeoutTimestamp;
+boolean isUnlockActionOnTimeout = false;
+
+// Bluetooth commands
 
 enum LockCommand {  LOCK_COMMAND = 'L',
                     UNLOCK_COMMAND = 'U',
@@ -61,13 +73,27 @@ void loop() {
   checkButtons();
 
   checkBluetoothMessages();
+
+  checkDoor();
 }
 
 void checkLock() {
-  if (isUpdatingLock) {
-    if (millis() - lockUpdateTimestamp >= LOCK_DELAY) {
-      isUpdatingLock = false;
+  if (isLockUpdating) {
+    if (millis() - lockUpdateTimestamp >= LOCK_UPDATE_DELAY) {
+      isLockUpdating = false;
       updateLockStatusColor();
+    }
+  }
+
+  if (isLockActionOnTimeout) {
+    if (millis() - lockActionTimeoutTimestamp >= LOCK_ACTION_TIMEOUT_DELAY) {
+      isLockActionOnTimeout = false;
+    }
+  }
+
+  if (isUnlockActionOnTimeout) {
+    if (millis() - unlockActionTimeoutTimestamp >= UNLOCK_ACTION_TIMEOUT_DELAY) {
+      isUnlockActionOnTimeout = false;
     }
   }
 }
@@ -96,8 +122,16 @@ void checkBluetoothMessages() {
   }
 }
 
+void checkDoor() {
+  //if (isDoorOpen) didOpenDoor(); --something like that
+}
+
+void didCloseDoor() {
+  lock();
+}
+
 void checkLockButton() {
-  if (isUpdatingLock) return;
+  if (isLockUpdating) return;
 
   button1State = digitalRead(button1Pin);
   button2State = digitalRead(button2Pin);
@@ -131,17 +165,29 @@ void sendBuzzNotification() {
 }
 
 void unlock() {
+  if(isLockActionOnTimeout) return;
+  
   lockUpdateTimestamp = millis();
-  isUpdatingLock = true;
+  isLockUpdating = true;
   tranca.write(0);
   state = UNLOCKED;
 }
 
 void lock() {
   lockUpdateTimestamp = millis();
-  isUpdatingLock = true;
+  isLockUpdating = true;
   tranca.write(150);
   state = LOCKED;
+}
+
+void timeOutLockAction() {
+  lockActionTimeoutTimestamp = millis();
+  isLockActionOnTimeout = true;
+}
+
+void timeOutUnlockAction() {
+  unlockActionTimeoutTimestamp = millis();
+  isUnlockActionOnTimeout = true;
 }
 
 void updateLockStatusColor() {
