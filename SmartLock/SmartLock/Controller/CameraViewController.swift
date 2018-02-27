fd096914ecad1b07f4b285bc0db25f5fbdf1da36
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2018-02-27 16:42:56 -0300

    dismiss the camera view after unlock the lock

--- SmartLock/SmartLock/Controller/CameraViewController.swift
+++ SmartLock/SmartLock/Controller/CameraViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class CameraViewController: UIViewController {
     
+    @IBOutlet weak var cameraView: UIImageView!
     @IBOutlet weak var lockButton: UIButton!
     @IBOutlet weak var lockStatusLabel: UILabel!
-    var lock: Lock?
+    private var lock: Lock?
+    private var wasUnlockedFromCamera = false
 
     @IBAction func dismiss(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -20,11 +22,13 @@ class CameraViewController: UIViewController {
     
     @IBAction func unlockButton(_ sender: Any) {
         lock?.unlock()
+        wasUnlockedFromCamera = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lock = UserModel.shared.selectedHome?.lock
+        loadImages()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,23 +37,46 @@ class CameraViewController: UIViewController {
         lock?.subscribe(observer: self) { [weak self] oldValue, newValue in
             self?.changeLockViews(to: newValue)
         }
-        
+        changeLockViews(to: (lock?.status)!)
+        cameraView.startAnimating()
+        wasUnlockedFromCamera = false
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
         lock?.unsubscribe(observer: self)
+        cameraView.stopAnimating()
     }
     
     private func changeLockViews(to status: LockStatus){
+        var statusLabel = ""
         switch status {
         case .locked:
-            lockStatusLabel.text = "Locked"
-            lockButton.imageView?.image = #imageLiteral(resourceName: "lock_button")
-        case .unlocked:
-            lockStatusLabel.text = "Unlocked"
+            statusLabel = "Locked"
             lockButton.imageView?.image = #imageLiteral(resourceName: "unlock_Button")
+        case .unlocked:
+            statusLabel = "Unlocked"
+            if wasUnlockedFromCamera{
+                Timer.scheduledTimer(withTimeInterval: 3, repeats: false, block: { (timer) in
+                    self.dismiss(animated: true, completion: nil)
+                })
+            }
+//            lockButton.imageView?.image = #imageLiteral(resourceName: "lock_button")
         }
+        
+        DispatchQueue.main.async {
+            self.lockStatusLabel.text = statusLabel
+        }
+    }
+    
+    func loadImages() {
+        var images: [UIImage] = []
+        for index in 1..<14{
+            images.append(UIImage(named: "Frame_\(index)")!)
+            print("Frame_\(index)")
+        }
+        cameraView.animationImages = images
+        cameraView.animationDuration = 1.3
     }
 }
