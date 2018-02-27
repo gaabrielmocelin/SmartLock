208bbdc4adf878286a67850806e3653eff734809
Max Zorzetti <maxzorzetti@live.com>
2018-02-27 14:29:59 -0300

    Adds door status label to RemoteUnlockVC

--- SmartLock/SmartLock/Controller/RemoteLockViewController.swift
+++ SmartLock/SmartLock/Controller/RemoteLockViewController.swift
@@ -14,6 +14,7 @@ class RemoteLockViewController: UIViewController {
     private var lock: Lock!
     
     @IBOutlet weak var lockStatusImageView: UIImageView!
+    @IBOutlet weak var lockStatusLabel: UILabel!
     
     @IBAction func lockAction(_ sender: Any) {
         lock.lock()
@@ -36,9 +37,11 @@ class RemoteLockViewController: UIViewController {
         super.viewWillAppear(animated)
         
         lock.subscribe(observer: self) { [weak self] oldValue, newValue in
-            self?.changeLockImageView(to: newValue)
+            self?.updateLockStatusImageView(to: newValue)
+            self?.updateLockStatusLabel(to: newValue)
         }
-        changeLockImageView(to: lock.status)
+        updateLockStatusImageView(to: lock.status)
+        updateLockStatusLabel(to: lock.status)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -47,12 +50,23 @@ class RemoteLockViewController: UIViewController {
         lock.unsubscribe(observer: self)
     }
     
-    private func changeLockImageView(to status: LockStatus) {
+    private func updateLockStatusImageView(to status: LockStatus) {
         switch status {
         case .locked:
             lockStatusImageView.image = #imageLiteral(resourceName: "locked_icon")
         case .unlocked:
             lockStatusImageView.image = #imageLiteral(resourceName: "unlocked_icon")
+        }
+    }
+    
+    private func updateLockStatusLabel(to status: LockStatus) {
+        switch status {
+        case .locked:
+            lockStatusLabel.text = "Locked"
+            lockStatusLabel.textColor = #colorLiteral(red: 0.2980392157, green: 0.8509803922, blue: 0.3921568627, alpha: 1)
+        case .unlocked:
+            lockStatusLabel.text = "Unlocked"
+            lockStatusLabel.textColor = #colorLiteral(red: 1, green: 0.5843137255, blue: 0, alpha: 1)
         }
     }
 }
