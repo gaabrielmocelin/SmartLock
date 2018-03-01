c2319b071962503bdf945c203703e962cb27f090
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-03-01 14:15:15 -0300

    Changes Mock home name to Beach House

--- SmartLock/SmartLock/Helpers/FakeDatabase.swift
+++ SmartLock/SmartLock/Helpers/FakeDatabase.swift
@@ -15,7 +15,7 @@ class FakeDatabase {
         let user = User(login: "", nickname: "João", password: "", phone: "999999999")
         let lock = Lock(name: "Front Door")
         let home = Home(id: "1", name: "Home", members: [user], lock: lock)
-        let mockHome = Home(id: "2", name: "Mock", members: [user], lock: MockLock(name: "2"))
+        let mockHome = Home(id: "2", name: "Beach House", members: [user], lock: MockLock(name: "Front Door"))
         user.homes = [home, mockHome]
         
         self.users = [user.login : user]
