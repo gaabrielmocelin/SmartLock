1ed9dc8d0b85b1d926ec8b3a20f1e78669c3c25f
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-03-05 15:57:45 -0300

    Add watchOS target

---
+++ SmartLock/SmartLockWatchOS Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SmartLockWatchOS Extension
+//
+//  Created by Matheus Vaccaro on 05/03/18.
+//  Copyright © 2018 Gabriel Mocelin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
