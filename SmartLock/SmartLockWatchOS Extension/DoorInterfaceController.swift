be112ce09813d380ca146f0f1b7d53114d5c93b4
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-03-07 16:03:46 -0300

    Add lock interface

--- SmartLock/SmartLockWatchOS Extension/DoorInterfaceController.swift
+++ SmartLock/SmartLockWatchOS Extension/DoorInterfaceController.swift
@@ -12,9 +12,32 @@ import WatchConnectivity
 
 class DoorInterfaceController: WKInterfaceController {
 
+    private var lockStatus: LockStatus! {
+        didSet {
+            didUpdate(lockStatus: lockStatus)
+        }
+    }
+    
+    private var isDisplayingCamera: Bool = false {
+        didSet {
+            lockStatusIcon.setHidden(isDisplayingCamera)
+            camera.setHidden(!isDisplayingCamera)
+            cameraButton.setTitle(isDisplayingCamera ? "Status" : "Camera")
+            updateImage(with: lockStatus)
+        }
+    }
+    
+    @IBOutlet var lockButton: WKInterfaceButton!
+    @IBOutlet var cameraButton: WKInterfaceButton!
+    
+    @IBOutlet var lockStatusIcon: WKInterfaceImage!
+    @IBOutlet var camera: WKInterfaceImage!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         print("AWAKE DOOR")
+        lockStatus = LockStatus.locked
+        isDisplayingCamera = false
         // Configure interface objects here.
     }
     
@@ -28,12 +51,67 @@ class DoorInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
    
-    @IBAction func didPressButton() {
+    @IBAction func didPressLockButton() {
         let session = WCSession.default
-        let command = LockCommand.unlock.rawValue
-        let message = [WatchLockMessageKey.name : "Front Door", WatchLockMessageKey.command : command]
-        let data = NSKeyedArchiver.archivedData(withRootObject: message)
-        session.sendMessageData(data, replyHandler: nil, errorHandler: nil)
+        if session.isReachable {
+            let command: LockCommand
+            switch lockStatus {
+            case .locked:
+                command = LockCommand.unlock
+                lockStatus = .unlocked
+            case .unlocked:
+                command = LockCommand.lock
+                lockStatus = .locked
+            default:
+                return
+            }
+            let message = [WatchLockMessageKey.name.rawValue : "Front Door", WatchLockMessageKey.command.rawValue : command.rawValue]
+            let data = NSKeyedArchiver.archivedData(withRootObject: message)
+            session.sendMessageData(data, replyHandler: nil, errorHandler: nil)
+        }
     }
     
+    @IBAction func didPressCameraButton() {
+        isDisplayingCamera = !isDisplayingCamera
+    }
+    
+    private func didUpdate(lockStatus: LockStatus) {
+        switch lockStatus {
+        case .locked:
+            self.lockButton.setTitle("Unlock")
+            self.lockButton.setEnabled(true)
+            self.lockButton.setBackgroundImage(UIImage(named:"blueBg"))
+            self.lockButton.setBackgroundColor(nil)
+            if !isDisplayingCamera {
+                updateImage(with: lockStatus)
+            }
+        case .unlocked:
+            self.lockButton.setTitle("Lock")
+            self.lockButton.setEnabled(true)
+            self.lockButton.setBackgroundImage(UIImage(named:"blueBg"))
+            self.lockButton.setBackgroundColor(nil)
+            if !isDisplayingCamera {
+                updateImage(with: lockStatus)
+            }
+        case .open:
+            self.lockButton.setTitle("...")
+            self.lockButton.setEnabled(false)
+            self.lockButton.setBackgroundImage(nil)
+            self.lockButton.setBackgroundColor(.gray)
+            if !isDisplayingCamera {
+                updateImage(with: lockStatus)
+            }
+        }
+    }
+    
+    private func updateImage(with lockStatus: LockStatus) {
+        switch lockStatus {
+        case .locked:
+            self.lockStatusIcon.setImage(UIImage(named: "IconLocked"))
+        case .unlocked:
+            self.lockStatusIcon.setImage(UIImage(named: "IconUnlocked"))
+        case .open:
+            self.lockStatusIcon.setImage(UIImage(named: "IconOpen"))
+        }
+    }
 }
---
+++ SmartLock/SmartLockWatchOS Extension/GradientLock.swift
@@ -0,0 +1,44 @@
+//
+//  GradientLock.swift
+//  SmartLockWatchOS Extension
+//
+//  Created by Matheus Vaccaro on 07/03/18.
+//  Copyright © 2018 Gabriel Mocelin. All rights reserved.
+//
+
+//import Foundation
+//import CoreFoundation
+//import CoreGraphics
+//import
+//import UIKit
+//
+//class Colors {
+//    func blueGradient(size: CGSize) -> UIColor {
+//
+//        let context = UIGraphicsGetCurrentContext()
+//
+//        let colorA = UIColor.blue
+//        let colorB = UIColor.red
+//
+//        let colorLocations: [CGFloat] = [0.0, 1.0]
+//        let colors: CFArray = [colorA.cgColor, colorB.cgColor] as CFArray
+//        let colorSpace = CGColorSpaceCreateDeviceRGB()
+//        let gradient = CGGradient(colorsSpace: colorSpace, colors: colors, locations: colorLocations)!
+//        
+//        context?.drawLinearGradient(gradient, start: .zero, end: CGPoint(x: 1, y: 1), options: CGGradientDrawingOptions.drawsBeforeStartLocation)
+//
+//
+//        UIGraphicsBeginImageContext(size)
+//        let color =
+//        UIGraphicsEndImageContext()
+//
+//        return colorA.cgColor
+//
+//
+//
+//
+//
+//        let backgroundGradientLayer =
+//    }
+//}
+
